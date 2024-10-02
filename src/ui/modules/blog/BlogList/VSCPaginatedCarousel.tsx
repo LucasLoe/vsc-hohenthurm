4faca8d02cf2dca7459200b95811f10fe87ca199
Lucas L <lucas_loe@gmx.de>
2024-10-02 21:07:26 +0200

    carousel footer

--- src/ui/modules/blog/BlogList/VSCPaginatedCarousel.tsx
+++ src/ui/modules/blog/BlogList/VSCPaginatedCarousel.tsx
@@ -1,24 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
 	Carousel,
 	CarouselContent,
 	CarouselItem,
 	CarouselNext,
 	CarouselPrevious,
+	type CarouselApi,
 } from '@/components/ui/carousel'
 
 import VSCPostPreview from '../VSCPostPreview'
 import BlueGeometric from '@/ui/backgrounds/BlueGeometric'
+import { ComponentInstanceIcon } from '@radix-ui/react-icons'
+import { FaVolleyball } from 'react-icons/fa6'
 
 type VSCPaginatedCarouselProps = {
 	posts: Sanity.BlogPost[]
 }
 
+const PageIndicator = ({
+	onClickFn,
+	active = false,
+}: {
+	onClickFn: (args: any) => void
+	active: boolean
+}) => {
+	return active ? (
+		<FaVolleyball className="size-4 text-white" />
+	) : (
+		<ComponentInstanceIcon
+			onClick={onClickFn}
+			className="size-4 cursor-pointer bg-none text-white"
+		/>
+	)
+}
+
 const VSCPaginatedCarousel = ({ posts }: VSCPaginatedCarouselProps) => {
+	const [api, setApi] = useState<CarouselApi>()
+	const [current, setCurrent] = useState(0)
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		if (!api) {
+			return
+		}
+
+		setCount(api.scrollSnapList().length)
+		setCurrent(api.selectedScrollSnap())
+
+		api.on('select', () => {
+			setCurrent(api.selectedScrollSnap())
+		})
+	}, [api])
+
+	const handleIndicatorClick = (index: number) => {
+		api?.scrollTo(index)
+	}
+
 	return (
-		<section className="py-4">
+		<section className="py-6 md:py-12">
 			<BlueGeometric>
 				<Carousel
+					setApi={setApi}
 					opts={{ align: 'start', loop: false }}
 					className="mx-auto w-full max-w-screen-xl"
 				>
@@ -37,6 +79,15 @@ const VSCPaginatedCarousel = ({ posts }: VSCPaginatedCarouselProps) => {
 					<CarouselPrevious className="hidden size-8 border-none bg-vsc-blue shadow-lg outline-none hover:bg-vsc-pink md:flex" />
 					<CarouselNext className="hidden size-8 border-none bg-vsc-blue shadow-lg outline-none hover:bg-vsc-pink md:flex" />
 				</Carousel>
+				<div className="mt-8 flex w-full justify-center gap-2">
+					{Array.from({ length: count }, (_, i) => (
+						<PageIndicator
+							onClickFn={() => handleIndicatorClick(i)}
+							key={i}
+							active={i === current}
+						/>
+					))}
+				</div>
 			</BlueGeometric>
 		</section>
 	)
