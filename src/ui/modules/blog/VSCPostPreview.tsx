bdb4ca7ef69e6e84f4c649e4484a5282cce80a4f
Lucas L <lucas_loe@gmx.de>
2024-10-02 21:07:26 +0200

    carousel footer

--- src/ui/modules/blog/VSCPostPreview.tsx
+++ src/ui/modules/blog/VSCPostPreview.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import processUrl from '@/lib/processUrl'
 import Img from '@/ui/Img'
-import Date from '@/ui/Date'
-import Categories from './Categories'
 import {
 	Card,
 	CardContent,
@@ -10,6 +8,43 @@ import {
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card'
+import { Badge } from '@/components/ui/badge'
+import { Roboto_Slab } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { ArrowRightIcon } from '@radix-ui/react-icons'
+
+const roboto = Roboto_Slab({ subsets: ['latin'], display: 'swap' })
+
+const FormattedDate = ({ value }: { value: string }) => {
+	const _date = new Date(value)
+	const formatter = new Intl.DateTimeFormat('de-DE', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	})
+
+	return <span>{formatter.format(_date)}</span>
+}
+
+const Categories = ({ categories }: { categories: Sanity.BlogCategory[] }) => {
+	console.log(categories)
+	if (!categories) return null
+
+	return (
+		<div className="flex place-items-center gap-x-1">
+			{categories.map((cat, idc) => (
+				<Badge
+					className="rounded-full border-gray-400 font-light text-gray-500"
+					key={idc}
+					variant="outline"
+				>
+					{cat.title}
+				</Badge>
+			))}
+		</div>
+	)
+}
 
 export default function VSCPostPreview({ post }: { post: Sanity.BlogPost }) {
 	return (
@@ -17,32 +52,36 @@ export default function VSCPostPreview({ post }: { post: Sanity.BlogPost }) {
 			className="group block space-y-2"
 			href={processUrl(post, { base: false })}
 		>
-			<Card className="h-[450px] rounded-none shadow-lg">
-				<CardHeader className="p-0">
-					<figure className="relative aspect-video overflow-hidden bg-ink/5">
+			<Card className="flex h-[450px] flex-col justify-between rounded-sm border-0 shadow-lg">
+				<CardHeader className="p-4 shadow-inner">
+					<figure className="relative aspect-video overflow-hidden rounded-t-sm bg-ink/5">
 						<Img
-							className="aspect-video w-full object-cover transition-[filter,transform] group-hover:scale-105 group-hover:brightness-110"
+							className="w-full object-cover transition-[filter,transform] group-hover:scale-105 group-hover:brightness-110"
 							image={post.metadata.image}
 							imageWidth={800}
 							alt={post.metadata.title}
 						/>
-
-						{post.featured && (
-							<span className="action absolute right-4 top-0 rounded-t-none py-1 text-xs shadow-md">
-								Featured
-							</span>
-						)}
 					</figure>
 				</CardHeader>
-				<CardContent>
-					<CardTitle className="h3 mt-4">{post.metadata.title}</CardTitle>
+				<CardContent className="flex grow flex-col justify-between">
+					<CardTitle className={cn('mb-2 text-xl', roboto.className)}>
+						{post.metadata.title}
+					</CardTitle>
+					<p>{post.metadata.description}</p>
+					<Button
+						variant="secondary"
+						size="sm"
+						className="mx-auto mb-0 mt-auto flex place-items-center gap-1 px-2 shadow"
+					>
+						<ArrowRightIcon />
+						Weiterlesen
+					</Button>
 				</CardContent>
-				<CardFooter>
-					<Date value={post.publishDate} />
-					<Categories
-						className="flex flex-wrap gap-x-2"
-						categories={post.categories}
-					/>
+				<CardFooter className="flex place-items-center justify-between pb-4">
+					<p className="text-sm font-light text-gray-400">
+						<FormattedDate value={post.publishDate} />
+					</p>
+					<Categories categories={post.categories} />
 				</CardFooter>
 			</Card>
 		</Link>
