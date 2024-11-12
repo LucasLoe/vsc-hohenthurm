a7e2bc882e9201d4ad4ace79ad207e1f9dab20ab
Lucas L <lucas_loe@gmx.de>
2024-11-12 09:31:30 +0100

    favicon

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -5,14 +5,11 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import VisualEditingControls from '@/ui/VisualEditingControls'
 import '@/styles/app.css'
-import NavMenu from '@/ui/navigation'
 import MainNavigationMenu from '@/ui/navigation/main-navigation-menu'
-import Menu from '@/ui/footer/Navigation'
-import Header from '@/ui/header'
 
 export const metadata: Metadata = {
 	icons: {
-		icon: `https://fav.farm/🖤`,
+		icon: `../favicon.ico`,
 	},
 }
 
@@ -23,14 +20,7 @@ export default async function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			{/* <GoogleTagManager gtmId='' /> */}
-
 			<body className="bg-vsc-bg-light text-vsc-bg-dark">
-				{
-					// <SkipToContent />
-					// <Announcement />
-					// <Header />
-				}
 				<MainNavigationMenu />
 				<main id="main-content" tabIndex={-1}>
 					{children}
