ad3a037b8c6a5f4df4bd111ba1a26b0e68911a58
Lucas L <lucas_loe@gmx.de>
2024-11-22 21:12:18 +0100

    christmas

--- src/ui/modules/Callout.tsx
+++ src/ui/modules/Callout.tsx
@@ -1,22 +1,44 @@
 import { PortableText } from 'next-sanity'
 import CTAList from '@/ui/CTAList'
 import { cn } from '@/lib/utils'
+import { ChristmasCallout } from './callouts/christmas'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { Mountains_of_Christmas } from 'next/font/google'
 
-const layoutClasses = {
-	normal: 'border-[1px] border-black',
-	christmas: 'bg-vsc-bg-dark text-white relative overflow-hidden',
-} as const
+const mountainsOfChristmas = Mountains_of_Christmas({
+	weight: ['400', '700'],
+	subsets: ['latin'],
+	display: 'swap',
+})
 
-const textClasses = {
-	normal: 'text-lg font-light',
-	christmas: 'text-lg font-light',
-}
-
-const Christmas = () => {
+const Christmaslayout = ({ href }: { href: string }) => {
 	return (
-		<p className="absolute -right-16 -top-[85%] fill-white text-[200px] text-white opacity-20">
-			❄
-		</p>
+		<section className="section">
+			<div className="relative mx-auto aspect-[1200/800] w-80 shadow-lg sm:w-[500px] lg:w-[700px]">
+				<ChristmasCallout />
+				<div className="absolute bottom-2 left-1/2 -translate-x-1/2 sm:bottom-4">
+					<div className="flex w-fit place-items-center gap-x-4 lg:gap-x-8">
+						<p
+							className={cn(
+								'text-nowrap font-bold sm:text-xl lg:text-3xl',
+								mountainsOfChristmas.className,
+							)}
+						>
+							Weihnachtsmarkt Hohenthurm 2024
+						</p>
+						<Link href={href}>
+							<Button
+								variant="outline"
+								className="border-[1px] px-1 py-0.5 text-xs sm:px-2 sm:text-sm"
+							>
+								Mehr Infos
+							</Button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</section>
 	)
 }
 
@@ -29,18 +51,28 @@ export default function Callout({
 	ctas: Sanity.CTA[]
 	variant: 'normal' | 'christmas'
 }>) {
+	if (variant === 'christmas') {
+		return (
+			<Christmaslayout
+				href={'https://vsc-hohenthurm.de/events/weihnachtsmarkt-2024'}
+			/>
+		)
+	}
+
 	return (
 		<section className="section">
 			<div
 				className={cn(
 					'section grid max-w-screen-lg items-center gap-12 gap-y-6 rounded bg-accent/5 md:grid-cols-[2fr,1fr]',
-					layoutClasses[variant || 'normal'],
+					'border-[1px] border-black',
 				)}
 			>
-				<div className={cn('richtext', textClasses[variant || 'normal'])}>
+				<div className="h-72 w-full">
+					<ChristmasCallout />
+				</div>
+				<div className={cn('richtext', 'text-lg font-light')}>
 					<PortableText value={content} />
 				</div>
-				{variant === 'christmas' && <Christmas />}
 				<CTAList ctas={ctas} />
 			</div>
 		</section>
